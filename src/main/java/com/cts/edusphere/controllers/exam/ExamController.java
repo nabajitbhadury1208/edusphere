1d081ebc66aae52de959acb4374d1b66e75e76c6
A <2479365@cognizant.com>
2026-03-09 18:04:10 +0530

    Added OnCreate and OnUpdate

--- src/main/java/com/cts/edusphere/controllers/exam/ExamController.java
+++ src/main/java/com/cts/edusphere/controllers/exam/ExamController.java
@@ -2,6 +2,8 @@ package com.cts.edusphere.controllers.exam;
 
 import com.cts.edusphere.common.dto.exam.ExamRequest;
 import com.cts.edusphere.common.dto.exam.ExamResponse;
+import com.cts.edusphere.common.validation.OnCreate;
+import com.cts.edusphere.common.validation.OnUpdate;
 import com.cts.edusphere.enums.Status;
 import com.cts.edusphere.exceptions.genericexceptions.ResourceNotFoundException;
 import com.cts.edusphere.services.exam.ExamService;
@@ -9,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,60 +26,53 @@ public class ExamController {
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> createExam(@RequestBody ExamRequest request){
-        try{
+    public ResponseEntity<?> createExam(@Validated(OnCreate.class) @RequestBody ExamRequest request){
+
             ExamResponse response = examService.createExam(request);
             return new ResponseEntity<>(response, HttpStatus.CREATED);
-        }catch (ResourceNotFoundException ex){
-            return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
 
-        }catch (Exception ex){
-            return new ResponseEntity<>("Unable to create exam", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
     }
 
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getAllExams(){
-        try{
+
             List<ExamResponse> exams=examService.getAllExams();
             return ResponseEntity.ok(exams);
-        }catch (Exception ex){
-            return new ResponseEntity<>("Error retrieving exams",HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+
+    }
+
+    @PutMapping
+    @PreAuthorize("hasRole('ADMIN)")
+    public ResponseEntity<?> updateExam(@PathVariable UUID id, @Validated(OnUpdate.class) @RequestBody ExamRequest request){
+        ExamResponse updatedExam = examService.updateExam(id,request);
+        return ResponseEntity.ok(updatedExam);
     }
 
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getExamById(@PathVariable UUID id){
-        try{
+
             ExamResponse exam=examService.getExamById(id);
             return ResponseEntity.ok(exam);
-        }catch (ResourceNotFoundException ex){
-            return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
-        }
+
     }
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> deleteExam(@PathVariable UUID id){
-        try{
+
            examService.deleteExam(id);
             return ResponseEntity.noContent().build();
-        }catch (ResourceNotFoundException ex){
-            return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
-        }
+
     }
 
 
     @GetMapping("/course/{courseId}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getExamsByCourse(@PathVariable UUID courseId){
-        try{
+
             List<ExamResponse> exams=examService.getExamsByCourse(courseId);
             return ResponseEntity.ok(exams);
-        }catch (Exception ex){
-            return new ResponseEntity<>("Error retrieving exams for course",HttpStatus.INTERNAL_SERVER_ERROR);
-        }
     }
 }
